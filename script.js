c5fdf027bb90742ee7d89f908f56f84eeefaf1ee
Owoeye Ayobami <42668217+aydotjs@users.noreply.github.com>
2021-12-12 13:48:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,4 +15,27 @@ navbarBtn.addEventListener('click', function(){
       
     }
 })
-console.log(overlay)
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const container = document.querySelector('.gallery-container');
+
+let counter = 0;
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+function nextSlide(){
+    container.animate([{opacity:'0.1'},{opacity:'1.0'}], {duration:1000, fill: "forwards"})
+    if(counter===34){
+        counter = 0
+    }
+    counter++;
+    container.style.backgroundImage = `url(slide-${counter}.jpg)`
+}
+function prevSlide(){
+    container.animate([{opacity:'0.1'},{opacity:'1.0'}], {duration:1000, fill: "forwards"})
+    if(counter===0){
+        counter = 35
+    }
+    counter--;
+    container.style.backgroundImage = `url(slide-${counter}.jpg)`
+}
